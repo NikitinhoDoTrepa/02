8e96067be7a87169ac24c7fb8a234d21100a145a
NikitinhoDoTrepa <5kleozinhmsd@gmail.com>
2024-11-26 15:09:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'quantos títulos  o time de futebol São Paulo tem internacionamente?',
+    '12'
+)
+
+criaCartao(
+    'futebol',
+    'quantos títulos  o time de futebol São Paulo tem em 2005?',
+    '3'
+)
+
+criaCartao(
+    'futebol',
+    'quantos jogos o Rogério Ceni jogos no São Paulo?',
+    '1237 jogos oficiais'
+)
+
+criaCartao(
+    'futebol',
+    'Quantos titulos Rogério Ceni conquistou pelo São Paulo?',
+    '18'
+)
